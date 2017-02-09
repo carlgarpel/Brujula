b93fcc1ec62101bfa98744a0bf693d653222c622
carlgarpel <cgarcia@aytoalmeria.es>
2017-02-09 14:52:31 +0100

    ++

--- www/src/Game2.js
+++ www/src/Game2.js
@@ -18,6 +18,8 @@ Ball.Game.prototype = {
 		this.ball.anchor.set(0.5);
 		this.physics.enable(this.ball, Phaser.Physics.ARCADE);
 
+ 		this.game.physics.arcade.enable(ball);
+
 		this.ball.body.gravity.x =  2;
 		this.ball.angle=90;
 
